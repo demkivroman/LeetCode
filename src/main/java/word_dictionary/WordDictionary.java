db8e2ada206f9fee1adef663d8b4532080152abf
demkivroman <demkivroman5@gmail.com>
2022-01-29 21:01:27 +0200

    word dictionary

---
+++ src/main/java/word_dictionary/WordDictionary.java
@@ -0,0 +1,34 @@
+package word_dictionary;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class WordDictionary {
+    Set<String> dictionary;
+
+    public WordDictionary() {
+        this.dictionary = new HashSet<>();
+    }
+
+    public void addWord(String word) {
+        if (word != null) {
+            dictionary.add(word);
+        }
+    }
+
+    public boolean search(String word) {
+        boolean isWord = false;
+        if (!word.contains(".")) {
+            return dictionary.contains(word);
+        }
+        Pattern pattern = Pattern.compile(word);
+        for (String el : dictionary) {
+            if (pattern.matcher(el).find()) {
+                isWord = true;
+                break;
+            }
+        }
+        return isWord;
+    }
+}
